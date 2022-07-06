08c782fd4b37c6c25a25f07520dd017c94ac642a
blessedjasonmwanza <mwanzabj@gmail.com>
2022-07-06 02:50:44 +0200

    Add migrations and devise authentication

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_category, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
@@ -65,6 +66,6 @@ class CategoriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.fetch(:category, {})
+      params.require(:category).permit(:name, :icon)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,11 @@
 class Category < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :transactions, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 255 }
+  has_one_attached :icon
+
+  def total_amount
+    transactions.sum(:amount)
+  end
 end
