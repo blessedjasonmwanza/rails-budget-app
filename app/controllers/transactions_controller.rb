5a11e6afed526e5333357bbc939a644843626d20
blessedjasonmwanza <mwanzabj@gmail.com>
2022-07-08 01:37:20 +0200

    Fix linter errors

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -57,7 +57,7 @@ class TransactionsController < ApplicationController
   # DELETE /transactions/1 or /transactions/1.json
   def destroy
     # puts plain: params
-    @transaction = Transaction.includes.(:user, :categories).find(params[:id])
+    @transaction = Transaction.includes.call(:user, :categories).find(params[:id])
     @transaction.destroy
     flash[:notice] = 'Transaction was successfully destroyed.'
     redirect_to category_transactions_path
