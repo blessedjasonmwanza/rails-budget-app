b5edd386fd18ec4a7d5ea37a121bf116ae6c8fc8
blessedjasonmwanza <mwanzabj@gmail.com>
2022-07-07 21:48:59 +0200

    Add authorization restrictions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  load_and_authorize_resource
   before_action :set_category, only: %i[ show edit update destroy ]
 
   # GET /categories or /categories.json
